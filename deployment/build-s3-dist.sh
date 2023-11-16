48a2e3c58d061268cde085428b5466613716ebea
Fernanda Gomes <79458567+fernandaGomesAquiris@users.noreply.github.com>
2023-11-16 17:13:19 -0300

    Add placeholder value to EUID Tags (#8)

--- deployment/build-s3-dist.sh
+++ deployment/build-s3-dist.sh
@@ -68,10 +68,13 @@ sed -i -e $replace $template_dist_dir/*.template
 replace="s/%%CONTACT_TAG%%/$4/g"
 echo "sed -i -e $replace $template_dist_dir/*.template"
 sed -i -e $replace $template_dist_dir/*.template
-replace="s/%%OWNER_TAG%%/$5/g"
+replace="s/%%EUID_TAG%%/$5/g"
 echo "sed -i -e $replace $template_dist_dir/*.template"
 sed -i -e $replace $template_dist_dir/*.template
-replace="s/%%ENV_TAG%%/$6/g"
+replace="s/%%OWNER_TAG%%/$6/g"
+echo "sed -i -e $replace $template_dist_dir/*.template"
+sed -i -e $replace $template_dist_dir/*.template
+replace="s/%%ENV_TAG%%/$7/g"
 echo "sed -i -e $replace $template_dist_dir/*.template"
 sed -i -e $replace $template_dist_dir/*.template
 
